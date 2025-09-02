2787de645b7ff79250550cbfe28e3cbe92aa8a21
009IsoAris7_ <159614599+Synthxyl704@users.noreply.github.com>
2025-09-02 17:03:07 +0530

    Update and rename usage_helper.h to headers/usage_helper.h

---
+++ headers/usage_helper.h
@@ -0,0 +1,6 @@
+#ifndef HEADERS_USAGE_HELPER_H
+#define HEADERS_USAGE_HELPER_H
+
+void printUsage(const char *programName);
+
+#endif // USAGE_HELPER_H
--- usage_helper.h
+++
@@ -1,6 +0,0 @@
-#ifndef USAGE_HELPER_H
-#define USAGE_HELPER_H
-
-void printUsage(const char *programName);
-
-#endif // USAGE_HELPER_H
